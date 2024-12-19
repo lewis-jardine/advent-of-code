6f6f2d39e549c01bf87df2cdac60f90d8592b53b
Lewis Jardine <lewisjardine777@gmail.com>
2024-12-19 20:58:53 +0000

    completed day 2

--- 2024/day2-red_nosed_reports/part2.py
+++ 2024/day2-red_nosed_reports/part2.py
@@ -1,15 +1,13 @@
 INPUT = "input.txt"
-# INPUT = "test_input.txt"
 
 
-def unsafe_level(report: list[int]) -> int | None:
+def report_safe(report: list[int]) -> bool:
     """
-    Find idx of first unsafe level in report or None if report is safe.
-    Report is safe if both:
+    Test report for safety, it is safe if both:
         - Any two adjacent levels differ by at least one and at most three.
         - The levels are either all increasing or all decreasing.
 
-    example report: [1, 4, 3] (idx 1 not safe)
+    example report: [1, 4, 3] (not safe)
     """
     increasing: bool | None = None
     for idx, n in enumerate(report):
@@ -21,7 +19,7 @@ def unsafe_level(report: list[int]) -> int | None:
         # Find level dif first
         dif = abs(n - next_n)
         if dif < 1 or dif > 3:
-            return idx
+            return False
 
         # Then find if decrease/ increase pattern holds. First run ignores this check
         if increasing is None:
@@ -30,9 +28,18 @@ def unsafe_level(report: list[int]) -> int | None:
 
         next_increasing = n < next_n
         if increasing != next_increasing:
-            return idx
+            return False
+    return True
 
-    return None
+
+def test_shortend_reports(report: list[int]) -> bool:
+    """Test if a report is safe when removing any letter"""
+    for n in range(len(report)):
+        copy_report = report.copy()
+        del copy_report[n]
+        if report_safe(copy_report):
+            return True
+    return False
 
 
 def main():
@@ -45,22 +52,8 @@ def main():
         report = line.strip().split()
         report = [int(n) for n in report]
 
-        idx = unsafe_level(report)
-        if idx is None:
+        if test_shortend_reports(report):
             safe_reports += 1
-            # print("safe")
-            continue
-
-        # Try remove the failed level and the next level in two different lists
-        # This accounts for last number being the one that should be removed
-        copy_report = report.copy()
-        del report[idx]
-        del copy_report[idx + 1]
-        if unsafe_level(report) is None or unsafe_level(copy_report) is None:
-            # print("safe by removing ", level)
-            safe_reports += 1
-        # else:
-        #     print("unsafe: ", orig_report, " tried removing ", idx)
 
     print(safe_reports)
 
